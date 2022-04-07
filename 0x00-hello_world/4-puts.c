d414576bb358a8b716cdd0937f036dbafa49acf0
FreiFrei <freweyni19@gmail.com>
2022-04-07 00:19:16 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints the phrase "programming is like building a multilingual puzzel
+ * Return: 0 if exited property, non-zero otherwise
  */
 int main(void)
 {
+	puts("\"programming is like building a multilingual puzzel");
 	return (0);
 } 
