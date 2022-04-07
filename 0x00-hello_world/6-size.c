6afd929676c2c3ad6f9a13bce7fec52b22d6049e
FreiFrei <freweyni19@gmail.com>
2022-04-07 09:54:28 -0700

    my commit quote message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,4 +13,4 @@ printf("size of a long int: %d byte(s)\n", sizeof(long int));
 printf("size of a long long int: %d byte(s)\n", sizeof(long long long int));
 print("size of a float: %d byte(s)\n", sizeof(float));
 return (0);
-} 
+}  
