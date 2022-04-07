0328d06ae0d7323e45edb44e8089c8d6843dd49a
FreiFrei <freweyni19@gmail.com>
2022-04-07 00:28:54 -0700

    the hellow puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "programming is like building a multilingual puzzel
+ * main - prints the phrase "programming is like building a multilingual puzzle
  * Return: 0 if exited property, non-zero otherwise
  */
-int main(void)
+int main(void
 {
 	puts("\"programming is like building a multilingual puzzel");
 	return (0);
-} 
+}  
