d91f03d76dadef67127e6d5e0b9d155ae8005718
FreiFrei <freweyni19@gmail.com>
2022-04-07 12:05:09 -0700

    my quete msg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  *main - prints exactly "and that piece of art is useful"
- * - dora korpar, 2015-10-19",
+ * - Dora Korpar, 2015-10-19",
  *Return: Always 0 (Success)
  */
 int main(void)
