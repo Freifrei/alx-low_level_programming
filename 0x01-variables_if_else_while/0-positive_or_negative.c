fb606eeb48e563fc500341d98ebf210f83dd309b
FreiFrei <freweyni19@gmail.com>
2022-04-11 06:57:07 -0700

    positive and negative commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- *main - entry point, returns zero, positive, negative based on a number
+ *main - Prints a random number and states whether
+ *       it is positive, negative, or zero.
  *
  *Return: Always 0 (Success)
  */
@@ -12,11 +14,12 @@ int n;
 
 stand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n == 0)
-	printf("%d is zero\n", n);
-else if (n > 0)
-	printf("%d is positive\n", n);
-else 
-	printf("%d is negative\n", n);
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
+
 return (0);
-} 
+}  
