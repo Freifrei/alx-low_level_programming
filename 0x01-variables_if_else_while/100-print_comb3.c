3991bbd3646572b78dbc2aee86fbc1735087e6a6
FreiFrei <freweyni19@gmail.com>
2022-04-11 10:54:41 -0700

    my print cpmbo msg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,14 +14,14 @@ int main(void)
 		for (c_two = c + 1; c_two <= 57; c_two++)
 		{
 			putchar(c);
-	          	putchar(c_two);
-		        if (c != 56 || c_two != 57)
-		        {
+			putchar(c_two);
+			if (c != 56 || c_two != 57)
+			{
 				putchar(44);
-		          	putchar(32);
-	         	}
-          	}
-        }
+				putchar(32);
+			}
+		}
+	}
 
 	putchar(10);
 	return (0);
