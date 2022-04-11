e7c622c7862c3aedbcefcd71dbcf7ac7072e64d2
FreiFrei <freweyni19@gmail.com>
2022-04-11 08:14:35 -0700

    my print alphabet commit msg

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
-putchar('\n\);
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+putchar(alpha);
+putchar('\n);
 return (0);
-}
+} 
