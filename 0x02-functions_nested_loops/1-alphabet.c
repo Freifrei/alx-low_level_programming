8dc73621ce19e3d08e3b284866ee265bd921874a
FreiFrei <freweyni19@gmail.com>
2022-04-12 01:46:37 -0700

    I sometimes suffer from insomina and i cant sleep i play what i call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * print_alphabet(void)
+ *
+ */
+void print_alphabet(void)
+{
+char i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+} 
