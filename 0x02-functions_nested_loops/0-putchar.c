a93405e61d12479a6ee28c2744d4cf89da0dba39
FreiFrei <freweyni19@gmail.com>
2022-04-12 05:41:52 -0700

    my putchar 0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,15 @@
  * Return: Always 0
  */
 int main(void)
-{	
-char str[] = "_putchar\n";
-int i = 0;
-
-while (str[i] != '\0')
 {
-chr c = str[i];
-_putchar(c);
-i++;
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+		_putchar(c);
+		i++;
+	}
+	return (0);
 }
-return (0);
-}  
