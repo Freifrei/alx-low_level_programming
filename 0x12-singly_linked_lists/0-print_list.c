dd04dc74b5e21a47f4bb2f0bb6ff1d4e89aa6d52
FreiFrei <freweyni19@gmail.com>
2022-05-20 09:49:01 -0700

    print lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -16,9 +15,11 @@ size_t print_list(const list_t *h)
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%u]s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
+
 	return (s);
-}
+
+} 
