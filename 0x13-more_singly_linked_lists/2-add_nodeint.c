21624ea58d13d20659365302099ce3cd40acaa84
FreiFrei <freweyni19@gmail.com>
2022-05-24 12:54:53 -0700

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,9 +15,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (!new)
 		return (NULL);
 
-	new->n =n;
+	new->n = n;
 	new->next = *head;
-	*head =new;
+	*head = new;
 
 	return (new);
 }
