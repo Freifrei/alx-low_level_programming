8a64b97d66120dae98ed436e499bf2358b46baac
FreiFrei <freweyni19@gmail.com>
2022-05-25 02:43:41 -0700

    list len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,8 +5,9 @@
  * list_len - returns the number of elements in a linked list
  * @h: pointer to the list_t list
  *
- * Return: number of elements in h
+ * Return: number of elements
  */
+size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
