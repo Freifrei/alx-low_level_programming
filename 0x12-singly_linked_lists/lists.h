df05c2285dd4bbab00664d4ccc8e8b3fb3fe2ed9
FreiFrei <freweyni19@gmail.com>
2022-05-25 01:33:12 -0700

    list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.h>
-#include <stdio.h>
-int _putchar(char c);
 
 /**
  * struct list_s - singly linked list
@@ -11,7 +8,6 @@ int _putchar(char c);
  * @next: points to the next node
  *
  * Description: singly linkedlist node structure
- * for ALX project
  */
 typedef struct list_s
 {
@@ -19,11 +15,11 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_s;
-char *create_array(unsigned int size, char c);
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif 
