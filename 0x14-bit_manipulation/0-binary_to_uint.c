910768fbd8f47f49baebdb4eb4af959668ace831
FreiFrei <freweyni19@gmail.com>
2022-05-27 10:29:51 -0700

    bin

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -44,4 +44,5 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 	}
 	return (n);
+
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,7 @@
 #define BYTE_LENGHT 8
 
 unsigned int binary_to_uint(const char *b);
+int _strlen(const char *s);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
@@ -12,4 +13,4 @@ int _atoi(const char *s);
 int _putchar(char c);
 int get_endianness(void);
 
-#endif
+#endif 
