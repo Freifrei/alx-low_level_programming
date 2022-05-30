66a889b423b32a3a3fabbe36ada8bbda1da2eee5
FreiFrei <freweyni19@gmail.com>
2022-05-30 16:21:52 -0700

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,40 +6,40 @@
  * @letters: the number of letters it should read and print
  * Return:  the actual number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	int s, t;
+	char *buf;
 
-int file;
-ssize_t rcount, wcount;
-char *buffer;
+	if (!filename)
+		return (0);
 
-if (filename == NULL)
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
 
-file = open(filename, O_RDWR);
-if (file == -1)
-	return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
 
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-{
-	free(buffer);
-	return (0);
-}
+	s = read(fd, buf, letters);
+	if (s < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[s] = '\0';
 
-rcount = read(file, buffer, letters);
-if (rcount == -1)
-	return (0);
+	close(fd);
 
-wcount = write(STDOUT_FILENO, buffer, rcount);
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
+	{
+		free(buf);
+		return (0);
+	}
 
-if (wcount == -1 || rcount != wcount)
-	return (0);
-
-free(buffer);
-
-close(file);
-
-return (wcount);
+	free(buf);
+	return (t);
 }
