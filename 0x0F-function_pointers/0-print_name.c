2aa688e1385e9774d16340bd3f90008a1d3502c6
Freifrei <freweyni19@gmail.com>
2022-07-08 07:06:18 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name
- * @name: name to print
+ * @name: string
  * @f: pointer to the printing function
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+	if ( != NULL)
+		f(name);
 }
