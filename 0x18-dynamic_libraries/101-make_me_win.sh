b531f1375e48dc7318acd5e3f61a9bbfef2a755b
Freifrei <freweyni19@gmail.com>
2022-07-11 13:52:07 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Freifrei/alx-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
+wget -q -o /tmp https://github.com/Freifrei/alx-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
