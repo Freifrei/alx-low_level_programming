fa4b5653b34cbedf0cff13a01b9c4ed9d0350141
Freifrei <freweyni19@gmail.com>
2022-07-12 00:59:18 -0700

    dynamic update

--- 0x18-dynamic_libraries/_strlen.c
+++ 0x18-dynamic_libraries/_strlen.c
@@ -1,1 +1,19 @@
-int _strlen(char *s);
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @: pointer to a string whose length is to be found
+ *
+ * Return: length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int i, len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -24,5 +25,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int _strlen(char *s);
 
 #endif /* MAIN_H */
