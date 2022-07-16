6b0f0185299b7db5704eba45df1f3ba6e8b7ce6c
Freifrei <Freweyni19@gmail.com>
2022-07-16 09:26:32 +0300

    resolve

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/*
+ *  @Auth :Yitagesu ketema
+ */
+#include <stdio.h>
+/**
+ *   main - starting point of our c programme
+ *     Return: 0
+ */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
